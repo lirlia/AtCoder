2041c5acd176b8601bb4f92b950055cac4c32f96
gin <10017674+lirlia@users.noreply.github.com>
2025-02-16 13:01:54 +0900

    add math_and_algorithm_a

---
+++ src/atcoder/math-and-algorithm/math_and_algorithm_a.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// TODO edit this code, this code is for https://atcoder.jp/contests/practice/tasks/practice_1
+
+	// param
+	var a int
+	fmt.Scanf("%d\n", &a)
+
+	// answer
+	fmt.Printf("%d\n", a+5)
+}
